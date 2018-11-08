7f54fcee6379fa53370237f10ab3cee0e84e496d
Valerie Soza <vsoza@u.washington.edu>
2018-11-07 17:28:23 -0800

    to get length of each scaffold in an assembly
    used this script for ordered scaffolds within each linkage group to create an AGP file for GenBank submission

---
+++ seq_length.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+#to get lengths of each scaffold for AGP file
+
+from Bio import SeqIO
+import sys
+cmdargs = str(sys.argv)
+for seq_record in SeqIO.parse(str(sys.argv[1]), "fasta"):
+ output_line = '%s\t%i' % \
+(seq_record.id, len(seq_record))
+ print(output_line)
